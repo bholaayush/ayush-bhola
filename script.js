361be995b444be16af15fbd1c64cbef09258e55c
utkarshbhola <63907963+utkarshbhola@users.noreply.github.com>
2023-08-20 22:19:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
+const toggleButton = document.getElementById('mobile-menu');
+const navbarMenu = document.getElementById('navbar-menu');
+
+toggleButton.addEventListener('click', () => {
+  navbarMenu.classList.toggle('active');
+});
