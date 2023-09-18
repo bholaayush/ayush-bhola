5bfdc13ff8c936f75e4f73d8d7262bc2c6bb4046
rtk-rnjn <ritik0ranjan@gmail.com>
2023-09-18 23:58:16 +0530

    fix shit

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-const toggleButton = document.getElementById('mobile-menu');
-const navbarMenu = document.getElementById('navbar-menu');
-
-toggleButton.addEventListener('click', () => {
-  navbarMenu.classList.toggle('active');
-});
+]
