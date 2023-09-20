8fcd9b51bb9961eab85e6f7ab6970ecb341263ad
utkarshbhola <63907963+utkarshbhola@users.noreply.github.com>
2023-09-20 12:32:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-]
+// Select all anchor links with the "smooth-scroll" class
+const anchorLinks = document.querySelectorAll('a[href^="#"]');
+
+// Add a click event listener to each anchor link
+anchorLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    // Prevent the default behavior of the link
+    e.preventDefault();
+
+    // Get the target element's ID from the link's href attribute
+    const targetId = link.getAttribute('href').substring(1);
+
+    // Find the target element by its ID
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      // Calculate the offset to scroll smoothly to the target element
+      const offset = targetElement.getBoundingClientRect().top;
+      
+      // Scroll smoothly to the target element
+      window.scrollTo({
+        top: offset + window.scrollY,
+        behavior: 'smooth'
+      });
+    }
+  });
+});
